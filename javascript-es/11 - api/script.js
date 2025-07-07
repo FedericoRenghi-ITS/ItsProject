72ff9b9f79c30a30bfd8c0a18c1782beeb0e9906
Federico Renghi <allievo_renghi.fe@itsumbria.it>
2025-07-07 14:58:53 +0200

    minor change, start working oon API connection

--- javascript-es/11 - api/script.js
+++ javascript-es/11 - api/script.js
@@ -1,3 +1,35 @@
+const baseURL="https://its-todo-api.azurewebsites.net/api"
+
+document.addEventListener("DOMContentLoaded", function(){
+  fetchData().then((todos) => {
+    if (todos) {
+      console.log("Dati ricevuti", todos);
+      renderTodos(todos)
+    }
+  })
+});
+
+
+
+
+/*async function fetchData() {
+  try{
+    const response= await fetch (`${baseURL}/Todo`,{
+      method: "GET",
+      headers: {
+      "x-api-key": "V4PaperYx4Ycc6zucdO6",
+    },
+  });
+  if (!response.ok) throw new Error('Errore nella risposta')
+
+  const todos =await response.json();
+  return todos;
+  }catch (error) {
+    console.error('Errore API:', error);
+    return null;
+  }
+}*/
+
 function fetchData() {
   fetch("https://its-todo-api.azurewebsites.net/api/Todo", {
     method: "GET",
